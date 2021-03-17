6305120fb6e1e9acd633dcd1f8b276cd45061dae
Jheavi <javierlaso213@hotmail.com>
2021-03-17 23:31:10 +0100

    added vue native app, not used yet

---
+++ mobile-app/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">{{ name }} Vue Native App</text>
+    <text-input v-model="name" id="name" />
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Javi'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
+
